12798abb7bf32a1ba4133c6b799a05a59a3751c9
darshan <darshann12@gmail.com>
2016-05-05 00:41:24 +0530

    added piedire a directive to draw pie chart

---
+++ client/src/widget/chart/piedire.js
@@ -0,0 +1,15 @@
+
+var pieLabels = ["January", "February", "March", "April", "May", "June", "July"];
+var pieData =[28, 48, 40, 19, 86, 27, 90];
+
+chartApp.directive('pieChart', createPieChart);
+function createPieChart() {
+      return {
+        scope: true,
+        controller: function($scope) {
+          $scope.labels =angular.copy(pieLabels);
+          $scope.data =angular.copy(pieData);
+          },
+        templateUrl:  'piechart.html'
+      }
+    }
